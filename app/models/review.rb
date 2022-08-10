7e797fa94290b7be0d98f1a941bc67bcb3576a67
Marving Moreton <moreton.marving@gmail.com>
2022-08-10 11:49:48 -0400

    updated version of Ruby syntax

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,8 @@ class Review < ApplicationRecord
   validates :rating, presence: true
 
   # A review’s rating must be a number between 0 and 5.
-  validates :rating, :inclusion => { :in => 0..5 }
+  # validates :rating, :inclusion => { :in => 0..5 }
   # A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
-  validates :rating, :numericality => { :only_integer => true, :message => " should be a number" }
+  # validates :rating, :numericality => { :only_integer => true, :message => " should be a number" }
+  validates :rating, numericality: { only_integer: true, in: (0..5) }
 end
